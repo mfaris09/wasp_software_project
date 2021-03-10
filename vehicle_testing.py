87ae2d06bfff16b6b4e561a6ab121589e1795f7a
Muhammad Faris <m.faris.ircham@gmail.com>
2021-03-10 11:49:45 +0700

    add test straight and straight fast
    The tests on straight has two version: standard and fast. Basically check if the vehicle moves in x-y position and maintain the same heading

--- vehicle_testing.py
+++ vehicle_testing.py
@@ -2,11 +2,11 @@ import os
 import numpy as np
 import unittest
 from simple_vehicle import SimpleVehicle
-
+import copy
 
 class testing_vehicle(unittest.TestCase):
 	
-	# 1.
+	# 1. Initial position x
 	def test_init_pos_x(self):
 		print('initial x-axis position test')		
 		env = SimpleVehicle()		
@@ -15,7 +15,7 @@ class testing_vehicle(unittest.TestCase):
 		self.assertGreater(self.observation[0],-10) 
 		self.assertLess(self.observation[0],10)
 
-	# 2.
+	# 2.Initial position y
 	def  test_init_pos_y(self):		
 		print('initial y-axis position test')			
 		env = SimpleVehicle()		
@@ -24,7 +24,7 @@ class testing_vehicle(unittest.TestCase):
 		self.assertGreater(self.observation[1],10) 
 		self.assertLess(self.observation[1],20)		
 
-	# 3.	 
+	# 3. Initial heading (radian)	 
 	def test_init_heading(self):
 		print('initial heading orientation test')	
 		env = SimpleVehicle()		
@@ -32,7 +32,7 @@ class testing_vehicle(unittest.TestCase):
 		
 		self.assertLess(self.observation[2],2*np.pi)		
 
-	# environment condition test
+	# 4. environment condition test
 	def test_env(self):
 		print('test vehicle environment')		
 		env  = SimpleVehicle()
@@ -44,6 +44,38 @@ class testing_vehicle(unittest.TestCase):
 		self.assertGreater(env.ylim[1], env.ylim[0])
 		
 		self.assertGreater(env.dt, 0.)
+	
+	# 5. move straight
+	def test_next_state_straight(self):		
+		print('test straight move')
+		env = SimpleVehicle()		
+		next_state = env.reset()
+		print(next_state)
+		init = copy.copy(next_state)
+		action = 2# move straight		
+		next_state, reward, done = env.discrete_step(action)
+		print(next_state)
+		print(init)
+				
+		self.assertNotEqual(init[0],next_state[0])	
+		self.assertNotEqual(init[1],next_state[1])	
+		self.assertEqual(init[2],next_state[2])							
+
+	# 6. move straight fast
+	def test_next_state_straight_fast(self):		
+		print('test straight move fast')
+		env = SimpleVehicle()		
+		next_state = env.reset()
+		print(next_state)
+		init = copy.copy(next_state)
+		action = 3# move straight fast		
+		next_state, reward, done = env.discrete_step(action)
+		print(next_state)
+		print(init)
+				
+		self.assertNotEqual(init[0],next_state[0])	
+		self.assertNotEqual(init[1],next_state[1])					
+		self.assertEqual(init[2],next_state[2])									
 
 if __name__ == '__main__':
     # begin the unittest.main()
