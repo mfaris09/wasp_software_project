ec37aaf08a855f5df0eca753c9bdb1d0e79fbc49
Muhammad Faris <m.faris.ircham@gmail.com>
2021-03-10 07:26:08 +0700

    add testing on initial vehicle's y-axis position and heading orientation

--- vehicle_testing.py
+++ vehicle_testing.py
@@ -15,6 +15,23 @@ class testing_vehicle(unittest.TestCase):
 		self.assertGreater(self.observation[0],-10) 
 		self.assertLess(self.observation[0],10)
 
+	# 2.
+	def  test_init_pos_y(self):		
+		print('initial y-axis position test')			
+		env = SimpleVehicle()		
+		self.observation = env.reset()
+		
+		self.assertGreater(self.observation[1],10) 
+		self.assertLess(self.observation[1],20)		
+
+	# 3.	 
+	def test_init_heading(self):
+		print('initial heading orientation test')	
+		env = SimpleVehicle()		
+		self.observation = env.reset()
+		
+		self.assertLess(self.observation[2],2*np.pi)		
+
 
 if __name__ == '__main__':
     # begin the unittest.main()
